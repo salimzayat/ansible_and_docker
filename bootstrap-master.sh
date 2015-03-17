e6c5a4fa373e7b7efd8d2b152f73c873323acb82
Salim Zayat (CFPB) <salim.zayat@cfpb.gov>
2015-03-17 13:50:20 -0400

    ansible installing docker on centos 6.5

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 # install ansible
-sudo apt-get install software-properties-common -yq
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update -yq
-sudo apt-get install ansible -yq
+# we can do this in two ways:
+# 1) via EPEL
+# 2) via pip
+##########
+# EPEL
+##########
+# sudo rpm -Uvh --quiet http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+
+##########
+# pip
+##########
+wget --no-check-certificate https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+sudo easy_install pip
+sudo yum install python-devel -yq
+sudo yum install gcc -yq
+sudo pip install ansible
+
+sudo mkdir -p /etc/ansible
 
 # Configure /etc/hosts file
 echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
@@ -13,7 +27,34 @@ echo "192.168.32.10   agent.example.com agent" | sudo tee --append /etc/hosts 2>
 echo "[local]" > /etc/ansible/hosts
 echo "192.168.32.10" >> /etc/ansible/hosts
 
-# configure a simple one-off ansible playbook to bootstrap docker
-/usr/bin/ansible-playbook -s install_docker.yaml
-# and a test
-/usr/bin/ansible-playbook -s test_docker.yaml
+# we need to configure the ssh handshake between the master and agent(s)
+# create the ~/.ssh directory
+if [ ! -d ~/.ssh/ ]; then
+	echo "creating a .ssh directory"
+	sudo mkdir ~/.ssh
+fi
+
+if [ ! -f ~/.ssh/known_hosts ]; then
+	echo "creating known hosts"
+	touch ~/.ssh/known_hosts
+fi
+
+if [ ! -f /vagrant/vagrant_rsa ]; then
+	echo "creating a new rsa cert"
+	ssh-keygen -t rsa -C "vagrant@master.example.com" -N '' -f /vagrant/vagrant_rsa
+fi
+
+if [ ! -f ~/.ssh/vagrant_rsa ]; then
+	echo "creating a new symlink to /vagrant/vagrant_rsa cert files"
+	sudo ln -fs /vagrant/vagrant_rsa ~/.ssh/id_rsa
+	sudo ln -fs /vagrant/vagrant_rsa.pub ~/.ssh/id_rsa.pub
+fi
+
+# # add it to ~/.ssh/known_hosts
+# if grep -q "192.168.32.10" ~/.ssh/known_hosts; then
+# 	echo "agent already added to known_hosts"
+# else
+# 	echo "adding agent to known_hosts"
+# 	ssh-keyscan 192.168.32.10 | tee -a ~/.ssh/known_hosts
+# 	cat ~/.ssh/known_hosts
+# fi
--- bootstrap-node.sh
+++ bootstrap-node.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-echo "nothing to do for provisioning agent node"
+# we need to handle all the ssh configuration between agent and master
+# to that end, make sure we have an ~/.ssh directory...
+if [ ! -d ~/.ssh/ ]; then
+	echo "creating a .ssh directory"
+	sudo mkdir ~/.ssh
+fi
+
+# and an authorized_keys files
+if [ ! -f ~/ssh/authorized_keys ]; then
+	echo "creating authorized_keys"
+	touch ~/.ssh/authorized_keys
+fi
+
+# if we have not yet added the master to the authorized keys, do so now by leveraging the vagrant_rsa public key that is in the shared directory
+if grep -q "vagrant@master.example.com" ~/.ssh/authorized_keys; then
+	echo "no need to add to authorized keys"
+else
+	echo "adding to authorized keys"
+	cat /vagrant/vagrant_rsa.pub >> ~/.ssh/authorized_keys
+fi
+
+# time to install some software:
+# cgroup
+sudo yum install -y libcgroup
+sudo sh /vagrant/cgroupfs-mount
+# selinux
+sudo yum install -y libselinux-python
